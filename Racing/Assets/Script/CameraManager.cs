20cf5ade1d922084bc070ec5e5f2f8bab6ae0ca6
csh <tizkzks96@gmail.com>
2019-07-24 03:39:42 +0900

    조이스틱 연동 + 카메라 임시 스크립트 + 임시 플레이어 지정

---
+++ Racing/Assets/Script/CameraManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+    public GameObject target;
+
+    private Transform targetTransform;
+
+	// Use this for initialization
+	void Start () {
+        targetTransform = target.transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(targetTransform.position.x, transform.position.y, targetTransform.position.z);
+	}
+}
---
+++ Racing/Assets/Script/Player/Controller/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody mRigidbody;
+
+    public Joystick joystick; //조이스틱 컨트롤러
+
+    public float moveSpeed; //나중에 자동차 정보 가져와서 적용해야함
+
+
+
+
+    private void Awake()
+    {
+        mRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        JoysticMove();
+    }
+
+    private void JoysticMove()
+    {
+        Vector3 moveVector = (Vector3.right * joystick.Horizontal + Vector3.forward * joystick.Vertical);
+
+        if (moveVector != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveVector);
+            mRigidbody.AddForce(transform.forward * moveSpeed);
+            //transform.Translate(moveVector * moveSpeed * Time.deltaTime, Space.World);
+            //mRigidbody.MovePosition(transform.position + moveVector * moveSpeed * Time.deltaTime);
+        }
+    }
+}
