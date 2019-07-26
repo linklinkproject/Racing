dff8a7241b34fac304618844761a51973be916ba
HyeYeon <gpdus335@naver.com>
2019-07-26 22:09:02 +0900

    StoreScene Button Menu 틀 구현
    데이터 베이스 만들고 누르면 아이템 나타나고 돌아가게 구현, 누르면 그에 따른 설명도 나오게 구현
    >> 각 아이템 데이터베이스에서 가져오도록 수정 할 것 각 아이템들을 전부 데이터베이스로 저장 할 것 .

---
+++ Racing/Assets/Scripts/Store/ItemData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "New Item/item")]
+
+public class ItemData : ScriptableObject {
+
+    public string itemName; // 아이템(자동차) 이름
+    [TextArea]
+    public string itemDesc; // 아이템 설명.
+    public Sprite itemImage; // 아이템 이미지.
+    public GameObject itemPrefab; // 아이템 프리팹.
+
+	
+}
---
+++ Racing/Assets/Scripts/Store/StoreMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoreMenu : MonoBehaviour {
+
+    // Test용데이터라 일단 SerializeField로 정보 받아왔음. >> 후에 각 아이템의 DB에서 정보를 가져올 예정.
+    [SerializeField]
+    private GameObject itemCar; // 각 버튼 클릭시 활성화 될 자동차
+    [SerializeField]
+    private GameObject itemDescArea; // 각 버튼 클릭시 활성화 될 자동차 설명창
+    [SerializeField]
+    private ItemData[] itemDatas; // 각각 저장해둔 아이템에 대한 정보들 (배열로 저장해서 불러오기)
+    [SerializeField]
+    private Text itemDescText; // 각 아이템에 대한 설명 텍스트
+
+
+	// Use this for initialization
+	void Start () {
+        itemCar.SetActive(false);
+        itemDescArea.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        itemCar.transform.Rotate(Vector3.up, 100f * Time.deltaTime);
+    }
+
+    public void ButtonOnClick ()
+    {
+        itemCar.SetActive(true);
+        itemDescArea.SetActive(true);
+        // AddItme(); >> 누른 버튼안의 정보를 담아야하는데 hitInfo로 넣을지 눌렀을 때 전송시킬지 고민중.
+        itemCar.transform.Rotate(Vector3.up, 100f * Time.deltaTime);
+    }
+
+    public void AddItme(ItemData _itemData) // >> 각 아이템 정보를 매개변수로 받아서 출력해주기. >> 일단은 테스트용
+    {
+        // itemDescText.text = _itemData.itemDesc; >> 아이템 정보를 전송 받았을 때,
+
+        // Test용 임시 데이터
+       
+    }
+
+}
