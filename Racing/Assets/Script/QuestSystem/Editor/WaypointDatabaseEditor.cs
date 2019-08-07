0deb5e704ac7f68596d1e72657035040ac1baa56
csh <tizkzks96@gmail.com>
2019-08-07 16:57:20 +0900

    CartoonFx 추가 + waypoint prepap 생성 + waypoint databases 추가

---
+++ Racing/Assets/Script/QuestSystem/Editor/WaypointDatabaseEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class WaypointDatabaseEditor{
+
+    private static string GetSavePath()
+    {
+        return EditorUtility.SaveFilePanelInProject("New waypoint database", "New waypoint database", "asset", "Create a new waypoint database.");
+    }
+
+    [MenuItem("Assets/Create/Databases/waypoint Database")]
+    public static void CreateDatabase()
+    {
+        string assetPath = GetSavePath();
+        WaypointDatabase asset = ScriptableObject.CreateInstance("WaypointDatabase") as WaypointDatabase;  //scriptable object
+        AssetDatabase.CreateAsset(asset, AssetDatabase.GenerateUniqueAssetPath(assetPath));
+        AssetDatabase.Refresh();
+    }
+}
