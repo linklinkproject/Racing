3ef46825e31c4fb318d5dfba577f436f1515ba0a
HyeYeon <gpdus335@naver.com>
2019-08-21 19:28:14 +0900

    스토어메뉴 스크립트 수정중
    수정수정 크리스탈

--- Racing/Assets/Scripts/Store/StoreMenu.cs
+++ Racing/Assets/Scripts/Store/StoreMenu.cs
@@ -42,19 +42,8 @@ public class StoreMenu : MonoBehaviour {
     {
 
         CarCheck(itemData);
-
-        //itemCar.SetActive(true);
-        //itemDescArea.SetActive(true);
-
-        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //RaycastHit hit;
-        //if (Physics.Raycast(ray, out hit))
-        //{
-        //    Debug.Log(hit.transform.name);
-        //}
-
-        // AddItme(); >> 누른 버튼안의 정보를 담아야하는데 hitInfo로 넣을지 눌렀을 때 전송시킬지 고민중.
-        itemCar.transform.Rotate(Vector3.up, 100f * Time.deltaTime);
+        
+       // itemCar.transform.Rotate(Vector3.up, 100f * Time.deltaTime);
     }
 
     public void CarCheck(ItemData _itemData)
@@ -66,13 +55,18 @@ public class StoreMenu : MonoBehaviour {
             Debug.Log(hit.transform.name); // 이건 잘됨. 여러군데서 시도해봤는데 위에 네 줄 다같이 있으면 무조건 되는 듯.
         }
 
-        if (hit.transform.name.Equals("TestCar1")) // 일단 아무 테스트객체 찾아볼라 그랬는데 아예 안돌아감.. 하... 계속 만져도 안돌아감 !!!!! 아악!!!!!!
+        if (hit.transform.name.Equals("TestCar1")) 
             // ray로 객체 이름 받아와서 이름 비교 해주는거 >> 이름 같으면 그 객체안의 item에서 data 받아와서 출력.
         {
-            itemData = _itemData; // 받아온 객체의 아이템정보로 바꿔줘서 출력해주기
-            itemDescText.text = itemData.itemDesc; // null 떠버림
+            // Debug.Log("TestCar1"); // 띄워짐.
 
             GameObject.Find("TestCars").transform.Find(hit.transform.name).gameObject.SetActive(true); //객체 이름 같은거 찾아서 띄워주기 
+
+            itemData = _itemData; // 받아온 객체의 아이템정보로 바꿔줘서 출력해주기
+            //itemDescText.text = "바보바보바보야";
+           // itemDescText.text = itemData.itemDesc + ""; // *** null 떠버림
+
+            //GameObject.Find("TestCars").transform.Find(hit.transform.name).gameObject.SetActive(true); //객체 이름 같은거 찾아서 띄워주기 
             GameObject.Find("TestCars").transform.Find(hit.transform.name).gameObject.transform.Rotate(Vector3.up, 100f * Time.deltaTime); // 빙글빙글
             
             //itemCar.SetActive(true);
